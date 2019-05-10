c2f6002af769622da3f9c3bf85e3ea711ee7b628
Ozobia Nwadibia <Ozobia_cn3@live.com>
2019-05-10 15:21:08 -0500

    Removing helper files and replacing with cdns

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,3 +1,41 @@
+ // When the user scrolls down 20px from the top of the document, show the button
+ window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    document.getElementById("btn").style.display = "block";
+  } else {
+    document.getElementById("btn").style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+function scrollUp() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+
+
+// // JQuery code for the tooltip (social media buttons)
+// $(document).ready(function() {
+//   $('[data-toggle="tooltip"]').tooltip();
+// });
+
+
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+
+
+
 //function to toggle body classes
 function changeBodyColor() {
   let toggleMode = document.getElementById("body");
@@ -15,3 +53,5 @@ function changeScrollButton() {
   let scrollButton = document.getElementById("scrollButton");
   scrollButton.style.color = scrollButton.style.color == 'white'? 'black':'white';
 }
+
+
