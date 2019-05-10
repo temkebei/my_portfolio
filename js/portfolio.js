1785a6e3e692e6f51ab08bcd73023ccd788a1ea0
Ozobia Nwadibia <Ozobia_cn3@live.com>
2019-05-10 14:43:26 -0500

    Tweaking toggle function for view mode

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,8 +1,17 @@
-function changeColor() {
+//function to toggle body classes
+function changeBodyColor() {
   let toggleMode = document.getElementById("body");
   toggleMode.classList.toggle("viewMode");
 }
 
+// function to toggle footer class
+function changeFooterColor() {
+  let footer = document.getElementById("footer");
+  footer.classList.toggle("footerMode");
+}
 
-
-
+// function to toggle scroll button class
+function changeScrollButton() {
+  let scrollButton = document.getElementById("scrollButton");
+  scrollButton.style.color = scrollButton.style.color == 'white'? 'black':'white';
+}
